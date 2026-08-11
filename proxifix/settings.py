8339932ce667487129f4993211620445ccfd8488
great-special <sirgreatspecial@gmail.com>
2026-08-11 23:05:12 +0100

    feat: Configure database settings to use dj-database-url for production

--- proxifix/settings.py
+++ proxifix/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +79,11 @@ WSGI_APPLICATION = 'proxifix.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default':dj_database_url.config(default=config('DATABASE_URL', default='sqlite:///db.sqlite3'), conn_max_age=600)
 }
 
 
